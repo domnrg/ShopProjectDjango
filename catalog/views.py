43e141778eef2aed9963e2f82f0deab02bc26e53
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-09-06 11:41:00 +0300

    Add utils.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,8 @@
-import csv
-import os
-
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import ContactForm
+from .utils import save_contact_to_file
+
 
 def home(request):
     """Обработка домашней страницы."""
@@ -19,16 +18,8 @@ def contacts(request):
             phone = form.cleaned_data['phone']
             message = form.cleaned_data['message']
 
-        # Определяем путь к файлу
-        file_path = os.path.join("data", "contacts.csv")
-
-        # Создаём папку data, если её нет
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-        # Записываем данные в CSV
-        with open(file_path, "a", newline="", encoding="utf-8") as f:
-            writer = csv.writer(f, delimiter=";")
-            writer.writerow([name, phone, message])
+        # Вызов функции сохранения
+        save_contact_to_file(name, phone, message)
 
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение сохранено.")
     else:
--- config/settings.py
+++ config/settings.py
@@ -10,22 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv(override=True)
 
 from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CONTACTS_DIR = BASE_DIR / "data"
+CONTACTS_FILE = CONTACTS_DIR / "contacts.csv"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!-0=38bo&2e)icpgb2w!9rohcf%16(8ziyr8#q@w6+0so3^^8+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == "True" else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -77,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
