514b8ebe357c047402084c349f011d2bb4d5abf8
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-09-15 10:38:24 +0300

    Add path to urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,6 +9,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
+    path("products/<int:pk>/", views.product_detail, name="product_detail"),
 ]
 
 if settings.DEBUG:
