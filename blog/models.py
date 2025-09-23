5c6a16f8261c03936f6e36eb4f2b1586eda615fd
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-09-23 10:02:54 +0300

    Fix models.py  in blog

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
     content = models.TextField(null=True, blank=True, verbose_name="Содержимое")
     image = models.ImageField(
         upload_to="images/", null=True, blank=True, verbose_name="Изображение"
     )
-    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
-    is_published = models.BooleanField(verbose_name="Признак публикации")
-    counter = models.IntegerField(verbose_name="Количество просмотров")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(default=True, verbose_name="Признак публикации")
+    counter = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
 
     def __str__(self):
         return self.title
@@ -17,4 +17,4 @@ class Post(models.Model):
     class Meta:
         verbose_name = "Пост"
         verbose_name_plural = "Посты"
-        ordering = ["title", "created_at"]
+        ordering = ["-created_at"]
