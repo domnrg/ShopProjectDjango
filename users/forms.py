eede7e57564f4f835f90124bdfb73b15c0d8abcf
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-10-05 13:52:36 +0300

    Add registration

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from users.models import User
 
 
-class UserRegisterForm(StyleFormMixin, UserCreationForm):
+class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
