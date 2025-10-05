e71e234627dcf16f70b43933a768d8146b0d84db
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-10-05 17:53:22 +0300

    Add mail_confirm

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UsersCreateView
+from users.views import UsersCreateView, email_verification
 
 app_name = UsersConfig.name
 
@@ -10,6 +10,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="logout.html"), name='logout'),
     path('register/', UsersCreateView.as_view(), name='register'),
-
+    path('email_confirm/<str:token>/', email_verification, name='email_confirm'),
 ]
 
