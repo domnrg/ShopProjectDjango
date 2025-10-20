5aef3eeb3d841c031a6805cd1a32ec1fefd25f90
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-10-20 12:04:39 +0300

    add url for ProductsByCategoryView

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
-from .views import ProductsListView, ProductDetailView, ContactFormView, HomeView, ProductCreateView, ProductUpdateView, ProductDeleteView, ProductUnpublishView
+from .views import ProductsListView, ProductDetailView, ContactFormView, HomeView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView, ProductUnpublishView, ProductsByCategoryView
 
 app_name = CatalogConfig.name
 
@@ -15,5 +16,6 @@ urlpatterns = [
     path("product/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
     path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
     path('product/unpublish/<int:pk>/', ProductUnpublishView.as_view(), name='product_unpublish'),
+    path('category/<int:category_id>/', ProductsByCategoryView.as_view(), name='products_by_category'),
 ]
 
